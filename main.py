c460e161459af12ea22e74be30bc072371ba55ff
Seokjin Yoon <sjy5386@gmail.com>
2021-09-19 16:36:52 +0900

    Update main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
 import string
+from typing import *
 
 import boto3
 
@@ -7,7 +8,7 @@ route53domains = boto3.client('route53domains', region_name='us-east-1', aws_acc
                               aws_secret_access_key='your secret key')
 
 
-def generate_names(character_pool, min_length, max_length):
+def generate_names(character_pool: Iterable, min_length: int, max_length: int):
     names = []
     for i in range(min_length, max_length + 1):
         names += list(
@@ -15,10 +16,16 @@ def generate_names(character_pool, min_length, max_length):
     return names
 
 
-def check_domain_availability(domain_name):
+def check_domain_availability(domain_name: str):
     return route53domains.check_domain_availability(DomainName=domain_name)['Availability'] == 'AVAILABLE'
 
 
 if __name__ == '__main__':
-    print(generate_names(string.ascii_lowercase + string.digits + '-', 2, 3))
-    print(check_domain_availability('example.com'))
+    domain_names = generate_names(string.ascii_lowercase + string.digits + '-', 2, 3)
+    top_level_domains = ['.com']
+    answer = []
+    for name in domain_names:
+        for tld in top_level_domains:
+            if check_domain_availability(name + tld):
+                answer.append(name + tld)
+                print(name + tld)
