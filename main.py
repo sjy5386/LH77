7194b667607e49397d2c613c1b08469729385110
Seokjin Yoon <sjy5386@gmail.com>
2021-09-19 16:25:13 +0900

    Add check_domain_availability

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import itertools
 import string
 
+import boto3
+
+route53domains = boto3.client('route53domains', region_name='us-east-1', aws_access_key_id='your access key',
+                              aws_secret_access_key='your secret key')
+
 
 def generate_names(character_pool, min_length, max_length):
     names = []
@@ -10,5 +15,10 @@ def generate_names(character_pool, min_length, max_length):
     return names
 
 
+def check_domain_availability(domain_name):
+    return route53domains.check_domain_availability(DomainName=domain_name)['Availability'] == 'AVAILABLE'
+
+
 if __name__ == '__main__':
     print(generate_names(string.ascii_lowercase + string.digits + '-', 2, 3))
+    print(check_domain_availability('example.com'))
